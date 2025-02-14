b6b6d70702388cadcc74815fb7f1b6910174709c
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-02-14 14:41:42 +0100

    refactor: Clean up utils.ts by removing unnecessary comments and improving import formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,6 @@
-/*
- * ############################################################################### *
- * Created Date: Fr Feb 2025                                                   *
- * Author: Boluwatife Olasunkanmi O.                                           *
- * -----                                                                       *
- * Last Modified: Fri Feb 14 2025                                              *
- * Modified By: Boluwatife Olasunkanmi O.                                      *
- * -----                                                                       *
- * HISTORY:                                                                    *
- * Date      	By	Comments                                                   *
- * ############################################################################### *
- */
-
-import { clsx, type ClassValue } from 'clsx';
-
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
