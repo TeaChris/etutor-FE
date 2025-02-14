5ca05127ebf3f334fc8e8942e7f1a6e3eb21db44
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-02-14 14:40:00 +0100

    feat: Create index.ts to export logo from navbar

---
+++ public/navbar/index.ts
@@ -0,0 +1,15 @@
+/*
+ * ############################################################################### *
+ * Created Date: Fr Feb 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Fri Feb 14 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+import logo from './logo.svg';
+
+export { logo };
