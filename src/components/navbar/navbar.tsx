455b201681ee314f4a3f04eaa72902e3b913531d
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-05 10:47:53 +0100

    feat: Enhance navbar with search, browse dropdown, and user action icons

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,17 @@
 import Link from 'next/link';
 import { MaxWidthWrapper } from '../max-width-wrapper';
 import Image from 'next/image';
-import { logo } from '@public/navbar';
+import { bell, heart, logo, shoppingCartSimple } from '@public/navbar';
+import {
+  Button,
+  buttonVariants,
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui';
 
 const topnav = [
   {
@@ -34,7 +44,10 @@ const Navbar = () => {
   return (
     <div className="w-screen h-[148px] space-y-0">
       <div className="bg-black w-full h-[45%]">
-        <MaxWidthWrapper className="h-full flex items-center justify-between">
+        <MaxWidthWrapper
+          className="h-full flex items-center justify-between"
+          isLarge
+        >
           <div className="flex items-center gap-x-7">
             {topnav.map((nav) => (
               <Link key={nav.label} href={nav.route}>
@@ -48,9 +61,80 @@ const Navbar = () => {
       </div>
 
       <div className="w-full h-[55%] bg-transparent border-b border-gray-500">
-        <MaxWidthWrapper className="h-full flex items-center justify-between">
+        <MaxWidthWrapper
+          className="h-full flex items-center justify-between"
+          isLarge
+        >
           <div className="flex items-center gap-x-7">
             <Image src={logo} alt="Logo" width={130} height={130} />
+
+            <Select>
+              <SelectTrigger className="w-28">
+                <SelectValue placeholder="Browse" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <div className="w-80 relative h-fit">
+              <Input placeholder="Search" className=" pl-8" />
+            </div>
+          </div>
+
+          <div className="flex items-center gap-x-0.5">
+            <Link
+              href="/sign-in"
+              className={buttonVariants({
+                variant: 'ghost',
+              })}
+            >
+              <Image src={bell} alt="Login" width={24} height={24} />
+            </Link>
+
+            <Link
+              href="/sign-in"
+              className={buttonVariants({
+                variant: 'ghost',
+              })}
+            >
+              <Image src={heart} alt="Login" width={24} height={24} />
+            </Link>
+
+            <Link
+              href="/sign-in"
+              className={buttonVariants({
+                variant: 'ghost',
+              })}
+            >
+              <Image
+                src={shoppingCartSimple}
+                alt="Login"
+                width={24}
+                height={24}
+              />
+            </Link>
+
+            <div className="flex gap-x-4">
+              <button className="px-2 py-0.5 rounded-sm bg-primary-100">
+                <Link
+                  href="/sign-in"
+                  className={buttonVariants({
+                    variant: 'link',
+                    className: 'no-underline',
+                  })}
+                >
+                  Create account
+                </Link>
+              </button>
+
+              <Button className="mr-4">
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+            </div>
           </div>
         </MaxWidthWrapper>
       </div>
