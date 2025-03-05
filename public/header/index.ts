06b9d5be84d003f1174efb0293a40ae827f0342c
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-05 11:55:38 +0100

    feat: Add header image export from public header module

---
+++ public/header/index.ts
@@ -0,0 +1,16 @@
+/*
+ * ############################################################################### *
+ * Created Date: We Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Wed Mar 05 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import headerImg from './header-img.png';
+
+export { headerImg };
