e29e852b1f1838dfc80fcff36563596776f082ef
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-05 13:50:37 +0100

    feat: Create about header image export module

---
+++ public/about/index.ts
@@ -0,0 +1,16 @@
+/*
+ * ############################################################################### *
+ * Created Date: We Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Wed Mar 05 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import aboutHeaderImg from './about-header-img.png';
+
+export { aboutHeaderImg };
