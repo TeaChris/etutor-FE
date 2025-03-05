d296b015b85fd47230bc780b1826dc0bb86b51d6
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-05 10:48:03 +0100

    feat: Create UI components index file for select, input, and button exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,16 @@
+/*
+ * ############################################################################### *
+ * Created Date: Fr Feb 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Fri Feb 14 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+export * from './select';
+export * from './input';
+export * from './button';
