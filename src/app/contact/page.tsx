46d8f0bcf90835381019dc368278405733d473da
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-06 09:34:24 +0100

    chore: Export Fit image for Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,8 @@
  * Copyright (c) 2025 Hq
  */
 
-import { MaxWidthWrapper, MinHeader } from '@/components';
+import { Button, Icons, MaxWidthWrapper, MinHeader } from '@/components';
+import { fit } from '@public/contact';
 import Image from 'next/image';
 
 const Page = () => {
@@ -27,17 +28,27 @@ const Page = () => {
               Customer Success Team to inquire about speaking events,
               advertising rates, or just say hello.
             </p>
+
+            <Button className="flex items-center gap-x-1.5">
+              <Icons.message size={17} className="mt-1" /> Copy email
+            </Button>
           </div>
 
           <div className="grid place-items-center w-1/2 h-full">
             <Image
-              src={aboutHeaderImg}
+              src={fit}
               alt="about header image"
               width={600}
               height={600}
               className="w-[500px] h-[400px] 2xl:w-[672px] 2xl:h-[500px] object-cover"
             />
           </div>
+        </MaxWidthWrapper>
+      </section>
+
+      <section className="w-full h-[682px]">
+        <MaxWidthWrapper className="grid place-items-center h-full">
+          <div className="flex gap-y-8 items-center"></div>
         </MaxWidthWrapper>
       </section>
     </main>
