b548d02e202e2975dc2ab5b33854e42569972699
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-06 10:11:07 +0100

    feat: Create authentication layout component

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+/*
+ * Filename: c:\Users\LENOVO\e-tutor\src\app\(auth)\layout.tsx
+ * Path: c:\Users\LENOVO\e-tutor
+ * Created Date: Thursday, March 6th 2025, 10:03:18 am
+ * Author: Boluwatife Olasunkanmi O.
+ *
+ * Copyright (c) 2025 Hq
+ */
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <main className="space-y-0 w-screen h-screen">{children}</main>;
+}
