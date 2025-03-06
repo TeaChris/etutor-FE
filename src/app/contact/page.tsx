f7900167be73e8ebe41bce628c6ceb239e3929e5
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-06 09:29:02 +0100

    feat: Implement Contact page layout with header and content section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,8 +7,41 @@
  * Copyright (c) 2025 Hq
  */
 
+import { MaxWidthWrapper, MinHeader } from '@/components';
+import Image from 'next/image';
+
 const Page = () => {
-  return <main>Page</main>;
+  return (
+    <main className="space-y-0 w-full">
+      <MinHeader label="Contact" title="Contact" />
+
+      <section className="w-full h-[660px]">
+        <MaxWidthWrapper className="flex items-center w-full h-full">
+          <div className="space-y-8 w-1/2">
+            <h3 className="text-6xl font-medium tracking-wide text-black">
+              Connect with us
+            </h3>
+
+            <p className="w-[30rem] text-base text-gray-300">
+              Want to chat? We’d love to hear from you! Get in touch with our
+              Customer Success Team to inquire about speaking events,
+              advertising rates, or just say hello.
+            </p>
+          </div>
+
+          <div className="grid place-items-center w-1/2 h-full">
+            <Image
+              src={aboutHeaderImg}
+              alt="about header image"
+              width={600}
+              height={600}
+              className="w-[500px] h-[400px] 2xl:w-[672px] 2xl:h-[500px] object-cover"
+            />
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
