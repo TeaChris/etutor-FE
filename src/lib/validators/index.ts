13b75751e9a811973a052038a2b9d226fdfe9953
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-06 17:42:00 +0100

    feat: Add Zod validation schema for create account form

---
+++ src/lib/validators/index.ts
@@ -0,0 +1,53 @@
+/*
+ * ############################################################################### *
+ * Created Date: Th Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Thu Mar 06 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+import * as z from 'zod';
+
+export const createAccountSchema = z.object({
+  firstName: z
+    .string()
+    .min(3, {
+      message: 'First name cannot be less than three characters',
+    })
+    .nonempty({ message: 'First name cannot be empty' }),
+  lastName: z
+    .string()
+    .min(3, { message: 'Last name cannot be less than three characters' })
+    .nonempty({ message: 'Last name cannot be empty' }),
+  username: z
+    .string()
+    .min(3, { message: 'Username cannot be less than three characters' })
+    .nonempty({ message: 'Username cannot be empty' }),
+  email: z
+    .string()
+    .email({
+      message: 'Please enter a valid email',
+    })
+    .regex(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      'Email must include the @ symbol',
+    )
+    .nonempty({ message: 'Email cannot be empty' }),
+  password: z
+    .string()
+    .regex(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@.!#$%&*])[A-Za-z\d@.!#$%&*]{8,}$/,
+      'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character (@, ., !, #, $, %, &, *)',
+    )
+    .nonempty({ message: 'Password cannot be empty' }),
+  termsAndConditions: z.boolean().refine((val) => val === true, {
+    message: 'You must accept the terms and conditions',
+  }),
+});
+
+export type TCreateAccountSchema = z.infer<typeof createAccountSchema>;
