fc9b2e0ee6936e22fa3e6bb6013deb121d148a1d
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-06 09:55:19 +0100

    feat: Enhance Contact page with branch locations and map section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@
  */
 
 import { Button, Icons, MaxWidthWrapper, MinHeader } from '@/components';
-import { fit } from '@public/contact';
+import { branch1, branch2, branch3, branch4, fit, map } from '@public/contact';
 import Image from 'next/image';
 
 const Page = () => {
@@ -16,7 +16,7 @@ const Page = () => {
     <main className="space-y-0 w-full">
       <MinHeader label="Contact" title="Contact" />
 
-      <section className="w-full h-[660px]">
+      <section className="w-full h-[540px]">
         <MaxWidthWrapper className="flex items-center w-full h-full">
           <div className="space-y-8 w-1/2">
             <h3 className="text-6xl font-medium tracking-wide text-black">
@@ -46,10 +46,49 @@ const Page = () => {
         </MaxWidthWrapper>
       </section>
 
-      <section className="w-full h-[682px]">
+      <section className="w-full h-[580px]">
         <MaxWidthWrapper className="grid place-items-center h-full">
-          <div className="flex gap-y-8 items-center"></div>
+          <div className="flex flex-col gap-y-6 items-center">
+            <h3 className="text-3xl font-semibold text-gray-900">
+              Our branches all over the world.
+            </h3>
+
+            <h6 className="w-[30rem] text-lg text-center text-gray-600">
+              Phasellus sed quam eu eros faucibus cursus. Quisque mauris urna,
+              imperdiet id leo quis, luctus auctor nisi.{' '}
+            </h6>
+
+            <div className="flex justify-between w-full">
+              {[branch1, branch2, branch3, branch4].map((img, i) => (
+                <div className="w-[23%] overflow-hidden h-[365px]" key={i}>
+                  <Image
+                    src={img}
+                    alt="branches image"
+                    width={700}
+                    height={700}
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </MaxWidthWrapper>
+      </section>
+
+      <section className="w-full h-[600px] bg-gray-50">
+        <MaxWidthWrapper className="flex flex-col items-center pt-12 space-y-8 h-full">
+          <h3 className="text-3xl font-semibold text-gray-900">Contact Us</h3>
+        </MaxWidthWrapper>
+      </section>
+
+      <section className="w-full h-[300px] overflow-hidden">
+        <Image
+          alt="map"
+          src={map}
+          width={700}
+          height={700}
+          className="object-cover w-full h-full"
+        />
       </section>
     </main>
   );
