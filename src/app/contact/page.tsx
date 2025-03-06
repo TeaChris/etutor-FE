fd78486af991e704793e89afbb72f5c9f37fd7a6
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-06 09:24:26 +0100

    feat: Create initial Contact page component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+/*
+ * Filename: c:\Users\LENOVO\e-tutor\src\app\contact\page.tsx
+ * Path: c:\Users\LENOVO\e-tutor
+ * Created Date: Thursday, March 6th 2025, 9:23:08 am
+ * Author: Boluwatife Olasunkanmi O.
+ *
+ * Copyright (c) 2025 Hq
+ */
+
+const Page = () => {
+  return <main>Page</main>;
+};
+
+export default Page;
