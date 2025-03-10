e33c844905d1c4a6cefff9c1435e3edd1aac07c8
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-10 19:05:42 +0100

    refactor: Change FormError component to use arrow function syntax and export it

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -4,11 +4,13 @@ interface FormErrorProps {
   message?: string;
 }
 
-export function FormError({ message }: FormErrorProps) {
+const FormError = ({ message }: FormErrorProps) => {
   return (
     <div className="flex items-center p-3 text-sm rounded-md bg-destructive/15 gap-x-2 text-destructive">
       <ExclamationTriangleIcon className="w-4 h-4" />
       <p>{message}</p>
     </div>
   );
-}
+};
+
+export { FormError };
