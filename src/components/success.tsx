990e5a0604f593be4c82a3de295e15be4d7df4c7
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-10 19:05:52 +0100

    refactor: Convert FormSuccess component to arrow function syntax and export it

--- src/components/success.tsx
+++ src/components/success.tsx
@@ -4,11 +4,13 @@ interface FormSuccessProps {
   message?: string;
 }
 
-export function FormSuccess({ message }: FormSuccessProps) {
+const FormSuccess = ({ message }: FormSuccessProps) => {
   return (
     <div className="flex items-center p-3 text-sm rounded-md bg-emerald-500/15 gap-x-2 text-emerald-500">
       <CheckCircledIcon className="w-4 h-4" />
       <p>{message}</p>
     </div>
   );
-}
+};
+
+export { FormSuccess };
