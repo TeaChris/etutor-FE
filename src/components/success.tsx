b574b070e7d13057aaf90b2c162937737bdffa91
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-10 18:59:25 +0100

    feat: Add FormSuccess component for displaying success messages

---
+++ src/components/success.tsx
@@ -0,0 +1,14 @@
+import { CheckCircledIcon } from '@radix-ui/react-icons';
+
+interface FormSuccessProps {
+  message?: string;
+}
+
+export function FormSuccess({ message }: FormSuccessProps) {
+  return (
+    <div className="flex items-center p-3 text-sm rounded-md bg-emerald-500/15 gap-x-2 text-emerald-500">
+      <CheckCircledIcon className="w-4 h-4" />
+      <p>{message}</p>
+    </div>
+  );
+}
