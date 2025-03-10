cfd4fbe78abecb26e3e782f4815b8683c25c86f4
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-10 16:50:48 +0100

    feat: Add index file to store module for session management

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+/*
+ * ############################################################################### *
+ * Created Date: Mo Mar 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Mon Mar 10 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+export * from './use-session';
