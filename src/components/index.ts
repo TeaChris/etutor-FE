ef58f6fd100db02bd86335ca8fb51b00d5489098
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-10 19:05:47 +0100

    feat: Export success and error components from index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@
  * Created Date: Fr Feb 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Wed Mar 05 2025                                              *
+ * Last Modified: Mon Mar 10 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -14,6 +14,8 @@ export * from './navbar';
 
 export * from './max-width-wrapper';
 export * from './min-header';
+export * from './success';
 export * from './icons';
+export * from './error';
 
 export * from './ui';
