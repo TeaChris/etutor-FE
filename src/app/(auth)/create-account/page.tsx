b09c2f010f4460483a171e7c0b1fab233a7914d1
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-17 19:00:23 +0100

    fix: Reorder imports in create-account page for better readability

--- src/app/(auth)/create-account/page.tsx
+++ src/app/(auth)/create-account/page.tsx
@@ -19,12 +19,15 @@ import {
   Label,
 } from '@/components';
 import { apple, createAccount, google } from '@public/auth';
-import Image from 'next/image';
+
+import { callApi, cn, createAccountSchema, TCreateAccountSchema } from '@/lib';
 
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { callApi, cn, createAccountSchema, TCreateAccountSchema } from '@/lib';
+
 import { useState } from 'react';
+
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 const Page = () => {
