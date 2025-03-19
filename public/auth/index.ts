740abcdd3a19eee0b31fbb3e088c34a330fd1682
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-19 15:14:16 +0100

    fix: Update last modified date and add sign-in image to auth exports

--- public/auth/index.ts
+++ public/auth/index.ts
@@ -3,7 +3,7 @@
  * Created Date: Th Mar 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Thu Mar 06 2025                                              *
+ * Last Modified: Wed Mar 19 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -15,5 +15,6 @@ import createAccount from './create-account.png';
 import google from './google.svg';
 import apple from './apple.svg';
 import facebook from './facebook.svg';
+import signIn from './signin.png';
 
-export { createAccount, apple, google, facebook };
+export { createAccount, apple, google, facebook, signIn };
