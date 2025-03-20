cdf84aed5af03d10051d5a62fcce658572dd4f7b
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-20 15:04:55 +0100

    feat: feat

---
+++ src/app/(students)/student/dashboard.page.tsx
@@ -0,0 +1,14 @@
+/*
+ * Filename: c:\Users\LENOVO\etutor\e-tutor\src\app\(students)\student\dashboard.page.tsx
+ * Path: c:\Users\LENOVO\etutor\e-tutor
+ * Created Date: Thursday, March 20th 2025, 2:57:08 pm
+ * Author: Boluwatife Olasunkanmi O.
+ *
+ * Copyright (c) 2025 PendulumHq
+ */
+
+const Page = () => {
+  return <main></main>;
+};
+
+export default Page;
