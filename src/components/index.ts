17272e51d1f41dd1d3998162b8ab93e6bf765493
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-05-14 16:41:40 +0100

    build: organised logic

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@
  * Created Date: Fr Feb 2025                                                   *
  * Author: Boluwatife Olasunkanmi O.                                           *
  * -----                                                                       *
- * Last Modified: Mon Mar 10 2025                                              *
+ * Last Modified: Thu Apr 03 2025                                              *
  * Modified By: Boluwatife Olasunkanmi O.                                      *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -17,5 +17,5 @@ export * from './min-header';
 export * from './success';
 export * from './icons';
 export * from './error';
-
+export * from './side-nav';
 export * from './ui';
